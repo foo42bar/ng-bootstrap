ed4bb7887d7a7469ffb1f1ee0602285b993b6ed2
Martin Ruhm <martin.ruhm@spiegel.de>
2019-07-22 23:45:13 +0200

    Revert "feat(typeahead): 1274 - provisional commented forcing the use of yarn"
    This reverts commit c659f6d1a8344697cd388186d12266a0a45d04c9.

--- misc/preinstall.js
+++ misc/preinstall.js
@@ -1,14 +1,13 @@
-// TODO 1274 mr1 2019-07: check whether forcing use of yarn is needed
-// if (process.env.npm_execpath.indexOf('yarn') === -1) {
-//   throw new Error(`
-//
-//   ###################################################
-//   #                                                 #
-//   #  Please use Yarn > 1.3 to install dependencies  #
-//   #                                                 #
-//   #  1. Install yarn (https://yarnpkg.com/)         #
-//   #  2. Run 'yarn' instead of 'npm install'         #
-//   #                                                 #
-//   ###################################################
-//   `);
-// }
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
+  throw new Error(`
+  
+  ###################################################
+  #                                                 #
+  #  Please use Yarn > 1.3 to install dependencies  #
+  #                                                 #
+  #  1. Install yarn (https://yarnpkg.com/)         #
+  #  2. Run 'yarn' instead of 'npm install'         #
+  #                                                 #
+  ###################################################
+  `);
+}
