c659f6d1a8344697cd388186d12266a0a45d04c9
Martin Ruhm <martin.ruhm@spiegel.de>
2019-07-22 18:19:29 +0200

    feat(typeahead): 1274 - provisional commented forcing the use of yarn

--- misc/preinstall.js
+++ misc/preinstall.js
@@ -1,13 +1,14 @@
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
-  throw new Error(`
-  
-  ###################################################
-  #                                                 #
-  #  Please use Yarn > 1.3 to install dependencies  #
-  #                                                 #
-  #  1. Install yarn (https://yarnpkg.com/)         #
-  #  2. Run 'yarn' instead of 'npm install'         #
-  #                                                 #
-  ###################################################
-  `);
-}
+// TODO 1274 mr1 2019-07: check whether forcing use of yarn is needed
+// if (process.env.npm_execpath.indexOf('yarn') === -1) {
+//   throw new Error(`
+//
+//   ###################################################
+//   #                                                 #
+//   #  Please use Yarn > 1.3 to install dependencies  #
+//   #                                                 #
+//   #  1. Install yarn (https://yarnpkg.com/)         #
+//   #  2. Run 'yarn' instead of 'npm install'         #
+//   #                                                 #
+//   ###################################################
+//   `);
+// }
